77d0c13cef44c2a6e23246d1da9bba95557ccb0e
jequezinho <jroverby92@gmail.com>
2020-05-22 11:41:13 -0500

    Submission

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
-
+  let imageId = 5306
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  const image = document.getElementById('image')
+  const name = document.getElementById('name')
+  const likes = document.getElementById('likes')
+
+  const commentSection = document.getElementById('comments')
+  const commentForm = document.getElementById('comment_form')
+  const submitButton = document.querySelector("input[type='submit']")
+
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(json => {
+    name.innerHTML = json.name
+    image.src = json.url
+    likes.innerHTML = json.like_count
+  })
+
+  document.addEventListener("click", e => {
+    e.preventDefault()
+
+    if (e.target.id === "like_button") {
+      likes.innerHTML = parseInt(likes.innerHTML) + 1
+
+      const obj = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({image_id: imageId})
+      }
+
+      fetch(likeURL, obj)
+    }
+
+    else if (e.target.className === 'delete-btn') {
+      const myComment = e.target.parentNode
+      const myCommentId = myComment["value"]
+
+      const obj = {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      }
+
+      fetch(`${commentsURL}/${myCommentId}`, obj)
+      .then(resp => resp.json())
+      .then(resp => myComment.remove())
+    }
+
+  })
+
+  submitButton.addEventListener("click", e => {
+    e.preventDefault()
+    const myComment = commentForm.querySelector("input[name='comment']")
+
+    const obj = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: myComment.value
+      })
+    }
+
+    fetch(commentsURL, obj)
+    .then(resp => resp.json())
+    .then(comment =>
+      commentSection.innerHTML += `<li value=${comment.id}>${myComment.value}<button class='delete-btn'>Delete</button></li>`
+    )
+  })
 
 })
