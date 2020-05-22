66b16103d6a04f93928e0e44ded9383029dc90ca
Gonzalez32 <3232rose@gmail.com>
2020-05-22 12:33:55 -0500

    code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+// document.addEventListener('DOMContentLoaded', () => {
+  // console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId =  `https://randopic.herokuapp.com/images/5333`
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,105 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+// })
+ 
+
+const div = document.querySelector('#image_card')
+div.addEventListener("click", function(event){
+  console.log(event.target)
+  if(event.target.className === "like_button") {
+    likes(event)
+  }
 })
+
+
+
+function fetchAPI() {
+  fetch(imageId)
+  .then(resp => resp.json())
+  .then(data => {
+    console.log('DATA', data)
+    renderData(data)
+  })
+}
+
+function renderData(data){
+  const img = `
+  <img src="${data.url}" id="${data.image}" data-id="${data.id}"/>
+  <h4 id="name">${data.name}</h4>
+  <span>Likes:
+    <span id="likes">${data.like_count}</span>
+  </span>
+  <button id="like_button">Like</button>
+  <form id="${data.comments}">
+    <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+    <input type="submit" value="Submit"/>
+  </form>
+  <ul id="${data.id}">
+       <!-- <li> for each comment goes here -->
+  </ul>
+  `
+
+  div.innerHTML = img
+} 
+
+function addLike(event) {
+  const countLike = parseInt(imgLikes.innerHTML) + 1
+  imgLikes.innerHTML = ''
+  imgLikes.innerHTML = countLike
+
+  const reqObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept' : 'application/json'
+    },
+    body: JSON.stringify( {
+      image_id: imageId
+    })
+  }
+
+  fetch(likeURL, reqObj)
+  .then(resp => resp.json())
+  // .then(Like => {
+  //   render(like)
+  // })
+}
+
+function comment(){
+  const form = document.querySelector('#comment_form')
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const form = event.target.comment.value
+    form.reset()
+    newComment(form)
+  })
+}
+
+function newComment(info){
+  const reqObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: imageId,
+      content: info
+    })
+  }
+
+  fetch(commentsURL, reqObj)
+  .then(resp => resp.json())
+  .then(comment => {
+    console.log(comment)
+  })
+}
+
+
+
+
+
+comment()
+fetchAPI()
+
